47afbfb9b4e996b1262344ce17c9259089c4d119
NoShenGG <decentvincent001@gmail.com>
2023-11-10 02:07:43 -0500

    Setup FMOD integration

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,18 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'test.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const TEST = "{2ab79860-442a-42b0-b956-380fa1dc9a89}"
+
+class Busses:
+	const MASTER_BUS = "{a4c296f8-f077-452b-8546-ea7f7bfb8858}"
+	const REVERB = "{cc9589d3-1926-4912-82a9-29aca17eb8ae}"
+
+class Banks:
+	const MASTER = "{e02a8011-03ec-4637-8ec4-9da710c89757}"
+
