78ba577fbfc2887a04a5e656b9ef0483154d12bd
NoShenGG <decentvincent001@gmail.com>
2024-02-01 21:04:54 -0500

    Add rotation to keyframes

--- Scripts/TimeMechanics/PositionKeyframe.cs
+++ Scripts/TimeMechanics/PositionKeyframe.cs
@@ -4,10 +4,12 @@ namespace Tip.Scripts.TimeMechanics;
 
 public class PositionKeyframe {
     public Vector3 position;
+    public Vector3 rotation;
     public double delta;
 
-    public PositionKeyframe(Vector3 position, double delta) {
+    public PositionKeyframe(Vector3 position, Vector3 rotation, double delta) {
         this.position = position;
+        this.rotation = rotation;
         this.delta = delta;
     }
 }
