68e61cef6074ec46c51484fbfa54c4c12e730501
LilG00s3 <ethankimmel134532@gmail.com>
2024-02-08 18:03:45 -0500

    box detector

--- Scripts/Keystone.cs
+++ Scripts/Keystone.cs
@@ -3,11 +3,23 @@ using Tip.Scripts.TimeMechanics;
 
 namespace Tip.Scripts;
 
-
 public partial class Keystone : Box
 {
 	public override void _Ready() {
 		base._Ready();
 		Position = new Vector3(-0.8f, 1f, -0.25f);
+	}
+
+	public void OnForcegrabberEntered(Node3D body) 
+	{
+		if (body is Keystone keystone)
+        {
+            GD.Print("Keystone was inserted into force grabber!");
+			Player player = GetNode<Player>("../Player");
+			player._DropItem();
+			this.Position = Vector3.Zero;
+
+        }
+		
 	}
 }
