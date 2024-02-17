eca0ecb1bf61a8f3afc3381b330453021a2a634c
LilG00s3 <ethankimmel134532@gmail.com>
2024-02-17 18:41:45 -0500

    Settings and loading

---
+++ Scripts/StartGame.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class StartGame : Control
+{
+	private VBoxContainer menu = null;
+
+	public override void _Ready()
+	{
+		menu = GetNode<VBoxContainer>("Menu");
+
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public void onStartButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/DebugScenes/LoadingScene.tscn");
+	}
+
+	public void onQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+
+	public void onSettingsPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/DebugScenes/Settings.tscn");
+	}
+
+
+}
